d9c46f5379e3ed7b9a76dc63b66fd76c75175d65
zDefcon <antholozadarequelme@gmail.com>
2025-12-14 04:02:37 -0500

    Agrega clase EmpleadoTiempoCompleto para gestión de empleados
    Se implementa la clase EmpleadoTiempoCompleto que extiende de Empleado.
    Esta clase incluye un atributo para el bono anual y métodos para calcular
    el salario anual y mostrar la información del empleado.

---
+++ src/main/java/Clases/EmpleadoTiempoCompleto.java
@@ -0,0 +1,19 @@
+package Clases;
+
+public class EmpleadoTiempoCompleto extends Empleado {
+    private double bonoAnual;
+
+    public EmpleadoTiempoCompleto(String nombre, int edad, double salarioBase, double bonoAnual) {
+        super(nombre, edad, salarioBase);
+        this.bonoAnual = bonoAnual;
+    }
+
+    public double calcularSalarioAnual() {
+        return (salarioBase  + bonoAnual);
+    }
+
+    @Override
+    public String mostrarInfo() {
+        return super.mostrarInfo() + ", Bono Anual: " + bonoAnual;
+    }
+}
